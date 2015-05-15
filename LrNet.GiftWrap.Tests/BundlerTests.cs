d168c17dc5a7ba2d70131a3413e483a3cb47f2e2
Leland Richardson <leland@tech.pro>
2015-05-14 19:53:46 -0700

    test project

---
+++ LrNet.GiftWrap.Tests/BundlerTests.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LrNet.GiftWrap.Tests
+{
+    [TestClass]
+    public class BundlerTests
+    {
+        //TODO:
+    }
+}
